02b03c96a128598d6495b2ae8d2f7af348bcf568
Thomas Hudson <snarfblam@gmail.com>
2018-01-10 18:49:56 -0500

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+/*
+
+Question:
+    Present question, list of answers, timer
+    User makes correct selection:
+        Display correct message and appropriate image/sound
+    User makes incorrect selection/timer runs out:
+        Display appropriate message and present correct answer
+
+        */
