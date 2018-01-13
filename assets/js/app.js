b46f88d923a5978187716bb9946b7eee3839dfdd
Thomas Hudson <snarfblam@gmail.com>
2018-01-13 13:45:55 -0500

    Finished basic game flow

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,14 +19,15 @@ Question:
 
 var TriviaGame;
 
-$(document).ready(function(){
+$(document).ready(function () {
     var game = {
 
         // Panes
         uiIntroPane: $("#intro-pane"),
         uiQuestionPane: $("#question-pane"),
         uiAnswerPane: $("#answer-pane"),
-        uiFinalResultPane: $("final-result-pane"),
+        uiFinalResultPane: $("#final-result-pane"),
+        uiPanes: [],
 
         uiStartButton: $("#start-button"),
         uiTimer: $("#timer"),
@@ -34,27 +35,60 @@ $(document).ready(function(){
         uiAnswerList: $("#answer-list"),
         uiAnswers: [],
 
-        uiAnswerResult: $("answer-result"),
-        uiCorrectAnswer: $("correct-answer"),
-        uiAnswerImage: $("answer-image"),
+        uiAnswerResult: $("#answer-result"),
+        uiCorrectAnswer: $("#correct-answer"),
+        uiAnswerImage: $("#answer-image"),
 
         uiTallyRight: $("#tally-right"),
         uiTallyWrong: $("#tally-wrong"),
 
 
-
+        // Per-game variables
         currentQuestion: null,
-        correctAnswerIndex: 0,
+        questionIndex: -1,
+        correctAnswerCount: 0,
+        incorrectAnswerCount: 0,
 
-        init: function() {
+        // Per-question variables
+        correctAnswerIndex: -1,
+        selectedAnswerIndex: -1,
+        guessMade: false,
+
+        timing: {
+            // delay after:
+            selectionMade: 1000,
+            answerShown: 5000,
+        },
+
+        init: function () {
             var answerItems = $(".answer-item");
-            this.uiAnswers.push($(answerItems[0]));
-            this.uiAnswers.push($(answerItems[1]));
-            this.uiAnswers.push($(answerItems[2]));
-            this.uiAnswers.push($(answerItems[3]));
+            this.initAnswerElement($(answerItems[0]));
+            this.initAnswerElement($(answerItems[1]));
+            this.initAnswerElement($(answerItems[2]));
+            this.initAnswerElement($(answerItems[3]));
 
-            this.displayQuestion(this.questions[0]);
-        },   
+            this.uiPanes = [
+                this.uiIntroPane,
+                this.uiQuestionPane,
+                this.uiFinalResultPane,
+                this.uiAnswerPane,
+            ];
+
+            this.displayIntroPane();
+
+            this.uiStartButton.click(this.startButton_click.bind(this));
+        },
+
+        /** Adds the specified element to this.uiAnswers and wires the click event */
+        initAnswerElement(jqElement) {
+            var self = this;
+            var index = this.uiAnswers.length;
+
+            this.uiAnswers.push(jqElement);
+            jqElement.click(function (e) {
+                self.uiAnswers_click(e, index);
+            });
+        },
 
         questions: [
             {
@@ -66,16 +100,106 @@ $(document).ready(function(){
                     "answer 4",
                 ]
             },
+            {
+                question: "second question",
+                answers: [
+                    "response 2",
+                    "response 3",
+                    "response 1",
+                    "*response 4",
+                ]
+            },
         ],
 
-        displayQuestion: function(question) {
-            this.uiQuestion.text(question.question);
+        uiAnswers_click: function (e, index) {
+            if (!this.guessMade) {
+                this.guessMade = true;
+                this.selectedAnswerIndex = index;
+                setTimeout(this.displayAnswer.bind(this), this.timing.selectionMade);
+            }
+        },
+
+        startButton_click: function (e) {
+            this.startQuiz();
+        },
+
+
+        startQuiz: function () {
+            this.questionIndex = -1;
+            this.correctAnswerCount = 0;
+            this.incorrectAnswerCount = 0;
+
+            if (!this.displayNextQuestion()) { // returns false if no more questions
+                this.endQuiz();
+            }
+        },
+
+        endQuiz: function () {
+            this.uiTallyWrong.text(this.incorrectAnswerCount);
+            this.uiTallyRight.text(this.correctAnswerCount);
+            this.setVisiblePane(this.uiFinalResultPane);
+        },
+
+        setVisiblePane: function (jqPane) {
+            for (var i = 0; i < this.uiPanes.length; i++) {
+                var pane = this.uiPanes[i];
+                if (pane == jqPane) {
+                    pane.removeClass("hidden");
+                } else {
+                    pane.addClass("hidden");
+                }
+            }
+        },
+
+        displayIntroPane: function () {
+            this.setVisiblePane(this.uiIntroPane);
+        },
+
+        /** Initializes and displays the next question. Returns false if there are no more questions, otherwise returns true. */
+        displayNextQuestion: function () {
+            this.guessMade = false;
+
+            this.questionIndex++;
+            if (this.questionIndex >= this.questions.length) return false; // no more questions
+
+            this.currentQuestion = this.questions[this.questionIndex];
+            this.displayQuestion(this.currentQuestion);
+
+            this.setVisiblePane(this.uiQuestionPane);
             
-            for(var i = 0; i < question.answers.length; i++) {
+            return true; // We found a question
+        },
+
+        displayAnswer: function () {
+            var self = this;
+
+            var correct = this.selectedAnswerIndex == this.correctAnswerIndex;
+            if (correct) {
+                this.uiAnswerResult.text("Correct!");
+                this.correctAnswerCount++;
+            } else {
+                this.uiAnswerResult.text("Wrong!");
+                this.incorrectAnswerCount++;
+            }
+            this.uiCorrectAnswer.text("The correct answer is: " + this.currentQuestion.answers[this.correctAnswerIndex]);
+
+            this.setVisiblePane(this.uiAnswerPane);
+            setTimeout(function(){
+                var moreQuestions = self.displayNextQuestion();
+                if(!moreQuestions){
+                    self.endQuiz();
+                }
+            }, this.timing.answerShown);
+        },
+
+        displayQuestion: function (question) {
+            this.uiQuestion.text(question.question);
+
+            for (var i = 0; i < question.answers.length; i++) {
                 var answer = question.answers[i];
 
                 // Asterisk marks the correct question
-                if(answer.charAt(0) == "*"){
+                if (answer.charAt(0) == "*") {
                     // Save the index and remove the asterisk
                     this.correctAnswerIndex = i;
                     answer = answer.substr(1);
