a1eb64bb818d17ef679d50efd59fc630ec90e0d9
Thomas Hudson <snarfblam@gmail.com>
2018-01-13 11:53:42 -0500

    Created basic page structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 /*
 
 Question:
@@ -8,3 +10,82 @@ Question:
         Display appropriate message and present correct answer
 
         */
+
+
+/*
+    Question object { question: string, answers[4]: string }
+        -The correct answer is prefixed with an asterisk
+*/
+
+var TriviaGame;
+
+$(document).ready(function(){
+    var game = {
+
+        // Panes
+        uiIntroPane: $("#intro-pane"),
+        uiQuestionPane: $("#question-pane"),
+        uiAnswerPane: $("#answer-pane"),
+        uiFinalResultPane: $("final-result-pane"),
+
+        uiStartButton: $("#start-button"),
+        uiTimer: $("#timer"),
+        uiQuestion: $("#question"),
+        uiAnswerList: $("#answer-list"),
+        uiAnswers: [],
+
+        uiAnswerResult: $("answer-result"),
+        uiCorrectAnswer: $("correct-answer"),
+        uiAnswerImage: $("answer-image"),
+
+        uiTallyRight: $("#tally-right"),
+        uiTallyWrong: $("#tally-wrong"),
+
+
+
+        currentQuestion: null,
+        correctAnswerIndex: 0,
+
+        init: function() {
+            var answerItems = $(".answer-item");
+            this.uiAnswers.push($(answerItems[0]));
+            this.uiAnswers.push($(answerItems[1]));
+            this.uiAnswers.push($(answerItems[2]));
+            this.uiAnswers.push($(answerItems[3]));
+
+            this.displayQuestion(this.questions[0]);
+        },   
+
+        questions: [
+            {
+                question: "sample question",
+                answers: [
+                    "answer 1",
+                    "*answer 2",
+                    "answer 3",
+                    "answer 4",
+                ]
+            },
+        ],
+
+        displayQuestion: function(question) {
+            this.uiQuestion.text(question.question);
+            
+            for(var i = 0; i < question.answers.length; i++) {
+                var answer = question.answers[i];
+
+                // Asterisk marks the correct question
+                if(answer.charAt(0) == "*"){
+                    // Save the index and remove the asterisk
+                    this.correctAnswerIndex = i;
+                    answer = answer.substr(1);
+                }
+
+                this.uiAnswers[i].text(answer);
+            }
+        },
+    };
+
+    TriviaGame = game;
+    TriviaGame.init();
+});
